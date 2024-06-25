ed3540f2b83b145a738bd0ca1da8668fe347f9ac
Soumyaranjan Panda <84384924+soumyaranjan-panda@users.noreply.github.com>
2024-06-25 06:07:58 +0530

    Time: 0 ms (100.00%), Space: 42 MB (39.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int pivot = pivot(nums);
-        int leftSide = BS(nums, target, 0, pivot-1);
-        if(leftSide != -1){
-            return leftSide;
-        }
-        return BS(nums, target, pivot, nums.length - 1);
-    }
-    static int BS(int[] arr, int target, int s, int e){
+    public int search(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
         while(s <= e){
             int m = s + (e - s)/2;
             if(arr[m] == target){
                 return m;
-            }else if(arr[m] > target){
-                e = m - 1;
+            }
+            if(arr[s] <= arr[m]){
+                if(target < arr[m] && target >= arr[s]){
+                    e = m - 1;
+                }else{
+                    s = m + 1;
+                }
             }else{
-                s = s + 1;
+                if(target > arr[m] && target <= arr[e]){
+                    s = m + 1;
+                }else{
+                    e = m - 1;
+                }
             }
         }
         return -1;
-    }
-    static int pivot(int[] arr){
-        int min = Integer.MAX_VALUE;
-        int pivot = 0;
-        int s = 0;
-        int e = arr.length - 1;
-        while(s <= e){
-            int m = s + (e - s)/2;
-            if(arr[m] >= arr[s]){
-                if (arr[s] <= min){
-                    min = arr[s];
-                    pivot = s;
-                }
-                s = m + 1;
-            }else{
-                if (arr[m] < min){
-                    min = arr[m];
-                    pivot = m;
-                }
-                e = m - 1;
-            }
-        }
-        return pivot;
     }
 }
